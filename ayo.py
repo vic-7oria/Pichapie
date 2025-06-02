e3b5bba66073bc28c2b956a187c8a965c46ceeef
Buldakkk <reyesqueencyruth@gmail.com>
2025-06-02 17:57:37 +0800

    Pichaprac

---
+++ ayo.py
@@ -0,0 +1,16 @@
+print(1 * 8)
+
+
+y = 35
+x = 6
+result = x*y
+print(result)
+
+# accessing contents of variable
+
+Name = "Queency Ruth"
+print(Name)
+
+print("Total length: ",  len(Name))
+
+print("Access the letter: ",  Name[8])
