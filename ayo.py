ac1716b298c1655afb01a059b0c6498114ba57db
Buldakkk <reyesqueencyruth@gmail.com>
2025-06-02 19:27:35 +0800

    added format strings

--- ayo.py
+++ ayo.py
@@ -16,3 +16,21 @@ print("Total length: ",  len(Name))
 print("Access the letter: ",  Name[8])
 
 print(Name[0:3])
+
+
+name = "Quee\n\"ncy \'Ruth"
+print("This is an experimental:", name)
+
+
+# Formatted strings
+
+first_Name = "Queency"
+last_Name = "Reyes"
+
+# Full = first_Name + " " + last_Name
+# Output: Queency Reyes
+
+# but we can also do this, it has same output
+Full = f"{first_Name} {last_Name}"
+
+print(Full)
