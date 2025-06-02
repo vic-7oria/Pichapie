2958528f1eb7ba4107f2e00b0cd8060d73a5e57e
Buldakkk <reyesqueencyruth@gmail.com>
2025-06-02 20:09:39 +0800

    type conversion added

--- ayo.py
+++ ayo.py
@@ -1,25 +1,25 @@
-print(1 * 8)
+# print(1 * 8)
 
 
 y = 35
 x = 6
 result = x*y
-print(result)
+# print(result)
+
 
 # accessing contents of variable
 
 Name = "Queency Ruth"
-print(Name)
+# print(Name)
 
-print("Total length: ",  len(Name))
+# print("Total length: ",  len(Name))
 
-print("Access the letter: ",  Name[8])
+# print("Access the letter: ",  Name[8])
 
-print(Name[0:3])
-
+# print(Name[0:3])
 
 name = "Quee\n\"ncy \'Ruth"
-print("This is an experimental:", name)
+# print("This is an experimental:", name)
 
 
 # Formatted strings
@@ -33,4 +33,26 @@ last_Name = "Reyes"
 # but we can also do this, it has same output
 Full = f"{first_Name} {last_Name}"
 
-print(Full)
+# print(Full)
+
+
+# String manipulation
+
+myName = "Queency \"GorGeous\" Reyes"
+
+'''
+print(myName.upper())
+print(myName.lower())
+print(myName.title())
+print(myName.find("GorG"))  # returns the index
+
+print("GorG" in myName)  # returns boolean
+'''
+
+# type conversion
+t = input("x: ")
+
+# we converted the string in the input so that the types are compatible
+y = int(t) + 1
+
+print(f"x: {t} y:{y}")
