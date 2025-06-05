a5a778792121bf700505a4f10016a834e3cb05cf
Buldakkk <reyesqueencyruth@gmail.com>
2025-06-05 21:48:24 +0800

    we used math module

---
+++ math_importing.py
@@ -0,0 +1,12 @@
+import math
+
+# Let's say we are to calculate the circumference of a circle
+# THE FORMULAR IS C = 2 PI R
+
+radius = int(input("Enter a radius: "))
+
+circum = 2 * math.pi * radius
+
+print(circum)
+
+# "math.pi" is imported from MATH module
