c62d2e1900344d13f905a68d6228a2124cff6f5b
Buldakkk <reyesqueencyruth@gmail.com>
2025-06-05 19:58:26 +0800

    we learned parameters and arguments

--- FUNCTIONS.py
+++ FUNCTIONS.py
@@ -1,5 +1,29 @@
-def greet():
-    print("Happy \n Birthday!")
+# myName is the Parameter
+def greet(myName):
+    print(f"Happy\n Birthday! \n {myName}")
 
 
-greet()
+# "Queency" is the Argument
+greet("Queency")
+
+
+# NOTE: Parameter is the input that you define (or declare ? ) for ur function, while Argument is the actual value of the Parameter
+
+# FUNCTION THAT DOES A TASK
+def getGreet(name):
+    return f"Nice to meet you, {name}!"
+
+
+message = getGreet("Kwens")
+print(message)
+
+
+# FUNCTION THAT CALCULATES
+def calc(number, by):
+    return number + by
+
+# to print the output of the function above, we need to store the arguments in a variable, and we can print that variable.
+
+
+result = calc(3, 5)
+print(result)
