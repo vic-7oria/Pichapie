2c1a84aca2df1a0df087a8b4b24687c76467efab
Buldakkk <reyesqueencyruth@gmail.com>
2025-06-06 20:22:33 +0800

    dollar peso converter
    unit = input("Dollar or Peso? (D or P): ")
    amount = float(input("Enter amount: "))
    
    if unit == "D".lower() or "D".upper():
        print(amount * 55.78)
    elif unit == "P".lower() or "P".upper():
        print(amount / 55.78)
    else:
        print("Invalid Unit") THERE WAS A MISTAKE IN THIS CODE, IT DOES NOT READ D OR P, IT CHECKS WHETHER THE CONDITIONS IN IF STATEMENT ARE TRUE THAT'S WHY IT EXECUTES THE FIRST IF, ALWAYS. bcoz it satisfies it since it results to true.

---
+++ Activity/Money Converter.py
@@ -0,0 +1,12 @@
+unit = input("Dollar or Peso? (D or P): ").upper() or input(
+    "Dollar or Peso? (D or P): ").lower()
+amount = float(input("Enter amount: "))
+
+if unit == "D":
+    print(amount * 55.78)
+elif unit == "P":
+    print(amount / 55.78)
+else:
+    print("Invalid Unit")
+
+# EZ PZ
