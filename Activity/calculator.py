6dc52b6b73237114e819211ad849e798ea2f60dd
Buldakkk <reyesqueencyruth@gmail.com>
2025-06-06 19:47:08 +0800

    act

---
+++ Activity/calculator.py
@@ -0,0 +1,16 @@
+operator = input("Enter the operator (+,-,*,/): ")
+inp1 = int(input("Enter the first number: "))
+inp2 = int(input("Enter the second number: "))
+
+if operator == "+":
+    print(inp1 + inp2)
+elif operator == "-":
+    print(inp1 - inp2)
+elif operator == "*":
+    print(inp1 * inp2)
+elif operator == "/":
+    print(inp1 / inp2)
+else:
+    print("Invalid operator :< Please rerun the code")
+
+# EZ PZ
