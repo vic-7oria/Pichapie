398b28fa90903ac39793dd1e434b69c804157fc9
Buldakkk <reyesqueencyruth@gmail.com>
2025-06-07 21:46:26 +0800

    edited the prints

--- indexing.py
+++ indexing.py
@@ -6,8 +6,18 @@ sample = "0123-567-9-10-11-12-48-55215"
 
 print(sample[0:25:2])
 
+print(sample[::4])
+
 # it will put the deafult, which is 0
 print(sample[:2])
 
 # it started to print from index 20 to the end
 print(sample[20:])
+
+# it printed the last digit
+print(sample[-1])
+
+# this basically counted from right to left, but the counting started with -1 instead of just 0
+# well maybe because 0 belongs to the first element from the left
+# yes, it makes sense
+print(sample[-8])
