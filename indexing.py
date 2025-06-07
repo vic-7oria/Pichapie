a93b63b6aee05910354dbf045b5219be2cf1b948
Buldakkk <reyesqueencyruth@gmail.com>
2025-06-07 21:33:24 +0800

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,13 @@
+# we access elements using []
+# [start : end : step]
+
+
+sample = "0123-567-9-10-11-12-48-55215"
+
+print(sample[0:25:2])
+
+# it will put the deafult, which is 0
+print(sample[:2])
+
+# it started to print from index 20 to the end
+print(sample[20:])
