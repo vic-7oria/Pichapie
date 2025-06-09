fc59a30c94a5cb54f24309a5ae19efa9958bed75
Buldakkk <reyesqueencyruth@gmail.com>
2025-06-09 20:07:09 +0800

    Update Timer

--- Activity/Timer.py
+++ Activity/Timer.py
@@ -31,11 +31,12 @@ print("Time's up!")
 # DIGITAL CLOCK type shi
 for z in range(timer, 0, -1):
     secs = z % 60  # 60 for idk really, Bro Code just put it w/o exp.
-    print(f"00:00:{secs:02}")
-
+    
     # to print 0 on the left of single digits. 0 is the padding and 2 means two digits as a format.
+    print(f"00:00:{secs:02}")
 
     # this acts as a delay in each seconds in the input, it puts each second into sleep.
     time.sleep(1)
 
 print("Time's up!")
+
